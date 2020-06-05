0362e57b6f961f075f0df414bb12701f7329b148
Adriano Hardtke <adr.hardtke@gmail.com>
2020-06-05 00:03:02 -0300

    Updates

---
+++ precache-manifest.6ec60819560c633c529300ce27371fc9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf4f2704b426bedc94a7ad9864254b9",
+    "url": "/save-the-wild/index.html"
+  },
+  {
+    "revision": "24f4d2993bafb268c741",
+    "url": "/save-the-wild/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "759c9881eb4a75c433fa",
+    "url": "/save-the-wild/static/css/main.21b81441.chunk.css"
+  },
+  {
+    "revision": "24f4d2993bafb268c741",
+    "url": "/save-the-wild/static/js/2.4c1d425d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/save-the-wild/static/js/2.4c1d425d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "759c9881eb4a75c433fa",
+    "url": "/save-the-wild/static/js/main.9071593f.chunk.js"
+  },
+  {
+    "revision": "900dd4fba4633690f80e",
+    "url": "/save-the-wild/static/js/runtime-main.ed380d26.js"
+  },
+  {
+    "revision": "a348d290e42f013e5821a99ec04d916e",
+    "url": "/save-the-wild/static/media/dropbox-paper.a348d290.svg"
+  },
+  {
+    "revision": "67a546700bd1f9f133e85b0f343cea52",
+    "url": "/save-the-wild/static/media/evernote.67a54670.svg"
+  },
+  {
+    "revision": "8559d93217a122b1ce29f62335602df0",
+    "url": "/save-the-wild/static/media/google-keep.8559d932.svg"
+  },
+  {
+    "revision": "1bbe1a138f8ee5993bcddbe0f9a185da",
+    "url": "/save-the-wild/static/media/notes.1bbe1a13.svg"
+  },
+  {
+    "revision": "96d6fd87cee6fe85ffc2734566e36a29",
+    "url": "/save-the-wild/static/media/one-note.96d6fd87.svg"
+  },
+  {
+    "revision": "135766abcbdc7538521dd750815ededb",
+    "url": "/save-the-wild/static/media/simplenote.135766ab.svg"
+  },
+  {
+    "revision": "184281f0fe87517a950beb8112c308dd",
+    "url": "/save-the-wild/static/media/tree-animated.184281f0.gif"
+  }
+]);
